e8190d0785daaee02c87df49c7d15c45dcaa05d2
Alexander Yee <alexnader@terrace.fi>
2023-03-21 00:04:46 -0400

    error handling

--- cogs/info_cog.py
+++ cogs/info_cog.py
@@ -7,6 +7,7 @@ from database.database import async_db_session
 from sqlalchemy.future import select
 from database.user_game import UserGame
 from database.game import Game
+from .utils.assets import CHARACTERS
 
 
 class InfoCog(commands.Cog):
@@ -15,35 +16,46 @@ class InfoCog(commands.Cog):
 
     @commands.command(name="userinfo", aliases=["memberinfo"])
     async def user_info(self, ctx, name: Optional[str]):
-        try:
-            user = None
+        user = None
 
-            if name is not None:
-                query = select(User).where(User.in_game_name == name)
-                result = (await async_db_session.execute(query)).first()
-                user = None if result is None else result[0]
-            else:
-                id = ctx.message.author.id
-                user = await User.get(id)
+        if name is not None:
+            query = select(User).where(User.in_game_name == name)
+            result = (await async_db_session.execute(query)).first()
+            user = None if result is None else result[0]
+        else:
+            id = ctx.message.author.id
+            user: User = await User.get(id)
 
-            if user is None:
-                await ctx.send("No information available for this user")
-                return
+        if user is None:
+            await ctx.send("No information available for this user")
+            return
 
-            embed = Embed(title="User information", timestamp=datetime.utcnow())
+        embed = Embed(title="User information", timestamp=datetime.utcnow())
 
-            fields = [("Name", user.in_game_name, True), ("Elo", user.elo, True)]
+        fields = [
+            ("Name", user.in_game_name, True),
+            ("Elo", user.elo, True),
+            (
+                "Current Character",
+                "Nobody" if user.current_character is None else user.current_character,
+                True,
+            ),
+        ]
 
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
 
-            await ctx.send(embed=embed)
-        except Exception as e:
-            await ctx.send(e)
+        await ctx.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("READY UP")
+    @commands.command()
+    async def characters(self, ctx):
+        await ctx.send("\n".join(CHARACTERS))
+
+    async def cog_command_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        await ctx.send(f"Error with info cog: {error}")
+        raise error
 
 
 # this setup function needs to be in every cog in order for the bot to be able to load it
--- cogs/user_cog.py
+++ cogs/user_cog.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from database.user import User
 from sqlalchemy import exc
+from .utils.assets import CHARACTERS
 
 
 class UserCog(commands.Cog):
@@ -23,20 +24,31 @@ class UserCog(commands.Cog):
             await ctx.send(f"Welcome {in_game_name}")
         except exc.IntegrityError:
             await ctx.send("A user already chose that name!")
-        except Exception as e:
-            await ctx.send(f"Error: {e}")
 
     @commands.command(aliases=["set", "setcharacter", "setCharacter"])
     async def set_character(self, ctx: commands.Context, character: str):
-        try:
-            id = ctx.message.author.id
-            user = await User.get(id)
-            if user == None:
-                ctx.send(
-                    "Please register yourself with $register <in_game_name> before setting a character"
-                )
-        except Exception as e:
-            await ctx.send(f"Error: {e}")
+        id = ctx.message.author.id
+        user = await User.get(id)
+        if user == None:
+            await ctx.send(
+                "Please register yourself with $register <in_game_name> first!"
+            )
+            return
+
+        if character.capitalize() not in CHARACTERS:
+            await ctx.send(
+                "Character does not exist, check playable characters with $characters"
+            )
+            return
+
+        await User.update(id, current_character=character)
+        await ctx.send("Character has updated!")
+
+    async def cog_command_error(
+        self, ctx: commands.Context, error: commands.CommandError
+    ):
+        await ctx.send(f"Error with user cog: {error}")
+        raise error
 
 
 # this setup function needs to be in every cog in order for the bot to be able to load it
